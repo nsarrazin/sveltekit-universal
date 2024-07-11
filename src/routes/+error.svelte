74b92817679f8dbee0af42f5842effaa7572df6d
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-07-11 18:40:29 +0200

    fix top padding on error page
    so it looks better on ios emulator

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,5 +2,5 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="mx-auto text-center text-3xl pt-5 font-bold">{$page?.error?.message}</h1>
+<h1 class="mx-auto text-center text-3xl pt-20 font-bold">{$page?.error?.message}</h1>
 <p class="mx-auto text-center">An error occurred.</p>
