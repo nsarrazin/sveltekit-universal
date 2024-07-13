08695d581176fa42542635eb29c28722c4eac767
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2024-07-13 11:03:04 +0200

    feat(front): add reload button on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
 	import { page } from '$app/stores';
 </script>
 
 <h1 class="mx-auto text-center text-3xl pt-20 font-bold">{$page?.error?.message}</h1>
 <p class="mx-auto text-center">An error occurred.</p>
+
+<button
+	class="btn btn-info mx-auto text-center"
+	on:click={async () => await invalidate('app:random')}
+>
+	<span class="btn-label"> Refresh </span>
+</button>
